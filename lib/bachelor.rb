157b4642b1a9fa0541d04c34b4d2b4c4a5440c12
Sarah Anderson <saranderson13@gmail.com>
2019-04-12 02:36:43 +0000

    Done.

--- lib/bachelor.rb
+++ lib/bachelor.rb
@@ -2,50 +2,38 @@ require 'pry'
 
 def get_first_name_of_season_winner(data, season)
   # returns the first name of the winner of the specified season
-  winner = ""
   
-  data.each do |season_info|
-    if season_info[0] == season
-      season_info[1].each { |contestant| winner = contestant["name"].split.first if contestant["status"] == "Winner" }
+  data.each do |season_num, contestant_hash|
+    if season_num == season
+      contestant_hash.each { |contestant| return contestant["name"].split.first if contestant["status"] == "Winner" }
     end
   end
-  winner
 end
 
 def get_contestant_name(data, occupation)
   # returns the name of the contestant with the specified occupation
-  lady_with_job = ""
   
-  data.each do |season_info|
-    season_info[1].each { |contestant| lady_with_job = contestant["name"] if contestant["occupation"] == occupation }
+  data.each do |season_num, contestant_hash|
+    contestant_hash.each { |contestant| return contestant["name"] if contestant["occupation"] == occupation }
   end
-  
-  lady_with_job
 end
 
 def count_contestants_by_hometown(data, hometown)
   # returns the total number of contestants from the specified hometown
   count = 0
   
-  data.each do |season_info|
-    season_info[1].each { |contestant| count += 1 if contestant["hometown"] == hometown }
+  data.each do |season_num, contestant_hash|
+    contestant_hash.each { |contestant| count += 1 if contestant["hometown"] == hometown }
   end
   count  
 end
 
 def get_occupation(data, hometown)
   # returns the occupation of the first contestant from the specified hometown
-  occupation = ""
   
-  data.each do |season_info|
-    season_info[1].each do |contestant|
-      if contestant["hometown"] == hometown
-        occupation = contestant["occupation"]
-        break
-      end
-    end
+  data.each do |season_num, contestant_hash|
+    contestant_hash.each { |contestant| return contestant["occupation"] if contestant["hometown"] == hometown }
   end
-  occupation
 end
 
 def get_average_age_for_season(data, season)
@@ -53,8 +41,8 @@ def get_average_age_for_season(data, season)
   ages = []
   age_total = 0
   
-  data.each do |season_info|
-    season_info[1].each { |contestant| ages << contestant["age"].to_f } if season_info[0] == season
+  data.each do |season_num, contestant_hash|
+    contestant_hash.each { |contestant| ages << contestant["age"].to_f } if season_num == season
   end
   
   ages.each { |age| age_total += age }
